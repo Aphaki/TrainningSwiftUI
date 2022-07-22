5977c2c3f10b10c431a61d4930610307670a0951
Aphaki <kiapha1108@gmail.com>
2022-07-22 09:02:42 +0900

    Haptic 부분 수정

--- SwiftfulThinkingContinuedLearning/HapticBootcamp.swift
+++ SwiftfulThinkingContinuedLearning/HapticBootcamp.swift
@@ -29,19 +29,24 @@ class HapticManager {
 struct HapticBootcamp: View {
     var body: some View {
         VStack(spacing: 15) {
-            Button("success".uppercased())
-            { HapticManager.shared.notification(feedbackType: .success) }
-            Button("error".uppercased())
-            { HapticManager.shared.notification(feedbackType: .error) }
-            Button("warning".uppercased())
-            { HapticManager.shared.notification(feedbackType: .warning) }
-            Divider()
-            Button("heavy".uppercased()) { HapticManager.shared.impact(style: .heavy) }
-            Button("medium".uppercased()) { HapticManager.shared.impact(style: .medium) }
-            Button("light".uppercased()) { HapticManager.shared.impact(style: .light) }
-            Button("rigid".uppercased()) { HapticManager.shared.impact(style: .rigid) }
-            Button("soft".uppercased()) { HapticManager.shared.impact(style: .soft) }
-            Divider()
+            Group {
+                Button("success".uppercased())
+                { HapticManager.shared.notification(feedbackType: .success) }
+                Button("error".uppercased())
+                { HapticManager.shared.notification(feedbackType: .error) }
+                Button("warning".uppercased())
+                { HapticManager.shared.notification(feedbackType: .warning) }
+            }
+            Divider().frame(height:1).background(.gray)
+            Group {
+                Button("heavy".uppercased()) { HapticManager.shared.impact(style: .heavy) }
+                Button("medium".uppercased()) { HapticManager.shared.impact(style: .medium) }
+                Button("light".uppercased()) { HapticManager.shared.impact(style: .light) }
+                Button("rigid".uppercased()) { HapticManager.shared.impact(style: .rigid) }
+                Button("soft".uppercased()) { HapticManager.shared.impact(style: .soft) }
+            }
+            Divider().frame(height:1).background(.gray)
+            Button("Selection".uppercased()) { HapticManager.shared.selection() }
         }
     }
 }
